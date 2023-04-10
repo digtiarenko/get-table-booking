73589bfd22f790919123220f2e38425762951270
Valentyn Digtiarenko <baggins999@gmail.com>
2023-04-10 18:33:38 +0300

    Added basic markup

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,5 @@ import styles from './page.module.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1 className="text-blue-400 text-7xl">Hello Next.js 13 !! </h1>
-    </main>
-  );
+  return <main></main>;
 }
